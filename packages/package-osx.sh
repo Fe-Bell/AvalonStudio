59961413ba47c79ac3d7bc7437aff4727ec18b47
Felipe Bellini <Fe.Bell@outlook.com>
2022-08-11 21:59:00 -0400

    Support for net6.0

--- packages/package-osx.sh
+++ packages/package-osx.sh
@@ -1,7 +1,7 @@
-dotnet publish -c Release -f net5.0 -r osx-x64 ../AvalonStudio/AvalonStudio
+dotnet publish -c Release -f net6.0 -r osx-x64 ../AvalonStudio/AvalonStudio
 rm -rf ./osx/AvalonStudio.app/Contents/MacOS
 mkdir ./osx/AvalonStudio.app/Contents/MacOS
-cp -R ../AvalonStudio/AvalonStudio/bin/Release/net5.0/osx-x64/publish/ ./osx/AvalonStudio.app/Contents/MacOS/
+cp -R ../AvalonStudio/AvalonStudio/bin/Release/net6.0/osx-x64/publish/ ./osx/AvalonStudio.app/Contents/MacOS/
 mkdir artifacts
 cd osx
 zip -r -X ../artifacts/AvalonStudio.osx-x64.zip *
